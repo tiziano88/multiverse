3af201feb0e0d911276456a1de7145eed8de0cf5
Tiziano Santoro <tzn@google.com>
2023-12-28 23:54:15 +0100

    Nix setup

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+    description = "linc2";
+    inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+      flake-utils.url = "github:numtide/flake-utils";
+      gomod2nix.url = "github:nix-community/gomod2nix";
+    };
+    outputs = { self, nixpkgs, flake-utils, gomod2nix } :
+      (flake-utils.lib.eachDefaultSystem
+        (system:
+          let
+            pkgs = import nixpkgs { 
+              inherit system;
+              overlays = [];
+            };
+          in {
+            devShell = 
+              pkgs.mkShell {
+                packages = [
+                ];
+                buildInputs = [
+                    pkgs.just
+                    pkgs.nodejs
+
+                    pkgs.nodePackages.prettier
+                    pkgs.nodePackages.tailwindcss
+                    pkgs.nodePackages.vue-cli
+
+                    pkgs.nodePackages.yo
+                    pkgs.nodePackages.generator-code
+                ];
+              };
+          }));
+}
