f0087e2ef31271027996819964b1e9c464526f7f
Tiziano Santoro <tzn@google.com>
2024-01-14 22:59:04 +0000

    Fix markdown preview

--- viewer/src/components/ArticleComponent.tsx
+++ viewer/src/components/ArticleComponent.tsx
@@ -5,8 +5,20 @@ import MarkdownPreview from "@uiw/react-markdown-preview";
 export const ArticleComponent: FieldViewer<multiverse.IArticle> = ({
   value,
 }) => {
+  // See https://github.com/uiwjs/react-markdown-preview?tab=readme-ov-file#disable-header-links
   return type("Article", [
     <div>{value?.title || ""}</div>,
-    <MarkdownPreview source={value?.body || ""} />,
+    <MarkdownPreview
+      source={value?.body || ""}
+      rehypeRewrite={(node: any, index, parent: any) => {
+        if (
+          node.tagName === "a" &&
+          parent &&
+          /^h(1|2|3|4|5|6)/.test(parent.tagName)
+        ) {
+          parent.children = parent.children.slice(1);
+        }
+      }}
+    />,
   ]);
 };
